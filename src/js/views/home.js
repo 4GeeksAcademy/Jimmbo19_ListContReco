45394c4acce1de0e837412635cb675270bbb14a1
Jimmbo19 <jega190112@gmail.com>
2024-09-29 20:14:46 +0000

    modal listo

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -31,8 +31,25 @@ export const Home = () => {
 							<Link to={`/modify/${item.id}`} >
 							<button>Modify</button>
 							</Link>
-							<button onClick={()=>actions.deletecontact(item.id)}>Eliminar</button>
-							
+							<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Eliminar</button>
+						
+								<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+								<div className="modal-dialog">
+									<div className="modal-content">
+									<div className="modal-header">
+										<h1 className="modal-title fs-5" id="exampleModalLabel">Are you sure?</h1>
+										<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+									</div>
+									<div className="modal-body">
+										If you delete this contact, the universe will go down?
+									</div>
+									<div className="modal-footer">
+										<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Oh no!</button>
+										<button onClick={()=>actions.deletecontact(item.id)}  type="button" className="btn btn-primary">Yes baby!</button>
+									</div>
+									</div>
+								</div>
+								</div>
 						</li>
 					);
 				})}
